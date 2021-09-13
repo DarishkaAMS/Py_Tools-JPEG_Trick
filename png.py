80613f4ae047f844d4b2829a3bc193a34270d4cd
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-09-13 20:34:21 +0300

    Subtract PNG from JPG file

--- png.py
+++ png.py
@@ -1,9 +1,20 @@
 from PIL import Image
 import io
 
-image = Image.open('cat_with_balloon.png')
-byte_arr = io.BytesIO()
-image.save(byte_arr, format="PNG")
+# Add PNG into JPG file
+# image = Image.open('cat_with_balloon.png')
+# byte_arr = io.BytesIO()
+# image.save(byte_arr, format="PNG")
+#
+# with open('cat_in_cup.jpg', 'ab') as file:
+#     file.write(byte_arr.getvalue())
 
-with open('cat_in_cup.jpg', 'ab') as file:
-    file.write(byte_arr.getvalue())
+# Subtract PNG from JPG file
+with open('cat_in_cup.jpg', 'rb') as test_image:
+    content = test_image.read()
+    offset = content.index(bytes.fromhex('FFD9'))
+    test_image.seek(offset + 2)
+
+    new_image = Image.open(io.BytesIO(test_image.read()))
+    new_image.save("new_Test_image.png")
+
