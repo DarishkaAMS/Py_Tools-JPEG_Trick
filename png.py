4a744a6ed221c6125f7001daaaa1d89884401629
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-09-13 20:47:12 +0300

    Subtract PNG from JPG file

--- png.py
+++ png.py
@@ -10,11 +10,11 @@ import io
 #     file.write(byte_arr.getvalue())
 
 # Subtract PNG from JPG file
-with open('cat_in_cup.jpg', 'rb') as test_image:
-    content = test_image.read()
-    offset = content.index(bytes.fromhex('FFD9'))
-    test_image.seek(offset + 2)
-
-    new_image = Image.open(io.BytesIO(test_image.read()))
-    new_image.save("new_Test_image.png")
+# with open('cat_in_cup.jpg', 'rb') as test_image:
+#     content = test_image.read()
+#     offset = content.index(bytes.fromhex('FFD9'))
+#     test_image.seek(offset + 2)
+#
+#     new_image = Image.open(io.BytesIO(test_image.read()))
+#     new_image.save("new_Test_image.png")
 
