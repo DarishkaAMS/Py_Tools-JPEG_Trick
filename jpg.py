3f51ef46daac1879bf7cd2ef19eb28a3de8deb26
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-09-13 20:12:13 +0300

    Add JPG modifier

---
+++ jpg.py
@@ -0,0 +1,9 @@
+# with open('cat_in_cup.jpg', 'ab') as image:
+#     image.write(b"Hello from DarishkaAMS")
+
+
+with open('cat_in_cup.jpg', 'rb') as image:
+    content = image.read()
+    offset = content.index(bytes.fromhex('FFD9'))
+    image.seek(offset + 2)
+    print(image.read())
